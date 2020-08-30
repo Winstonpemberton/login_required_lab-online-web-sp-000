b0945f65f80052a24db3fceba4f5f01370400b00
Winston Pemberton <winstonpemberton@gmail.com>
2020-08-30 17:30:41 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,12 +1,13 @@
 class SecretsController < ApplicationController
+  before_action :require_login
 
   def show
-    if params[:name].present?
-      session[:name] = params[:name]
-      redirect_to '/show'
-    else
-      redirect_to '/login'
-    end
   end
 
+  private
+  def require_login
+    unless session.include? :name
+      redirect_to :login
+    end
+  end
 end
